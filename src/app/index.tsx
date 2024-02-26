9b5d3ade7b960bab18af07ddd648b40ba41df294
VitorHugoAntunes <vitor4ntunes@gmail.com>
2024-02-26 16:30:14 -0300

    Add SectionList, Product Component and UseRef with ScrollAnimation

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,28 @@
 import { CategoryButton } from '@/components/Category-button';
 import { Header } from '@/components/Header';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, SectionList } from 'react-native';
 
-import { CATEGORIES } from '@/utils/data/products';
-import { useState } from 'react';
+import { CATEGORIES, MENU } from '@/utils/data/products';
+import { useState, useRef } from 'react';
+import { Product } from '@/components/Product';
 
 export default function Home() {
   const [category, setCategory] = useState(CATEGORIES[0]);
 
+  const sectionListRef = useRef<SectionList>(null);
+
   function handleCategorySelected(selectedCategory: string) {
     setCategory(selectedCategory);
+
+    const sectionIndex = CATEGORIES.findIndex((category) => category === selectedCategory)
+
+    if (sectionListRef.current) {
+      sectionListRef.current.scrollToLocation({
+        animated: true,
+        sectionIndex, // sectionIndex: sectionIndex
+        itemIndex: 0,
+      })
+    }
   }
 
   return (
@@ -24,6 +37,22 @@ export default function Home() {
         showsHorizontalScrollIndicator={false}
         className='max-h-10 mt-5'
         contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />
+
+      <SectionList
+        ref={sectionListRef}
+        sections={MENU}
+        keyExtractor={(item) => item.id}
+        stickySectionHeadersEnabled={false}
+        renderItem={({ item }) =>
+          <Product data={item} />
+        }
+        renderSectionHeader={({ section: { title } }) =>
+          <Text className='text-xl text-white font-heading mt-8 mb-3'>{title}</Text>
+        }
+        className='flex-1 p-5'
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
       />
     </View>
   )
